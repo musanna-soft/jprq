d17fdb3521d28b1c8cb8cc5d873b0047dfcc51e3
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2026-06-11 09:39:57 +0500

    chore(release): bump installers to v1.2 (TCP keepalive)

--- server/static/install.sh
+++ server/static/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-URL_PREFIX="https://github.com/musanna-soft/jprq/releases/download/v1.1"
+URL_PREFIX="https://github.com/musanna-soft/jprq/releases/download/v1.2"
 INSTALL_DIR=${INSTALL_DIR:-/usr/local/bin}
 
 case "$(uname -sm)" in
